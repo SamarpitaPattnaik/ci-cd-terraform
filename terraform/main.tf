ffe56d17bf21516752f253d20b13b6890de648b5
SamarpitaPattnaik <samarpita.pattnaik@hcltech.com>
2026-04-19 12:07:29 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,15 +4,15 @@ provider "aws" {
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "demo-eks-cluster"
-  cluster_version = "1.29"
+  cluster_name    = "terraform-eks-cluster"
+  cluster_version = "1.35"
 
-  subnet_ids = ["subnet-xxxx", "subnet-yyyy"]
-  vpc_id     = "vpc-xxxx"
+  subnet_ids = ["subnet-00f21c8a15164983f", "subnet-037b475270b0e713e","subnet-0ebe331755e4e9e38"]
+  vpc_id     = "vpc-0027db6152b73fc0d"
 
   eks_managed_node_groups = {
     default = {
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.small"]
       desired_size   = 2
     }
   }
