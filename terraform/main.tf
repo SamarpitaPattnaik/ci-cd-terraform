3cce1f27189b4285a67b8d15404d7295f52bddaf
SamarpitaPattnaik <samarpita.pattnaik@hcltech.com>
2026-04-19 13:28:50 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,7 @@ module "eks" {
   version = "~> 20.0"
   cluster_name    = "terraform-eks-cluster"
   cluster_version = "1.35"
+  create_cloudwatch_log_group = false
 
   subnet_ids = [
     "subnet-00f21c8a15164983f",
