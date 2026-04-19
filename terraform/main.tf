931eb9ad1aefaaec8b409e368f3db04ce3b6ce9e
SamarpitaPattnaik <samarpita.pattnaik@hcltech.com>
2026-04-19 14:12:42 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,11 +5,11 @@ provider "aws" {
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
+
   cluster_name    = "terraform-eks-cluster"
   cluster_version = "1.35"
-  create_cloudwatch_log_group = false
-  enable_kms_key = false
-  
+
+  create_cloudwatch_log_group = false   # ✅ keep this
 
   subnet_ids = [
     "subnet-00f21c8a15164983f",
@@ -23,7 +23,7 @@ module "eks" {
     default = {
       instance_types = ["t3.small"]
       desired_size   = 2
-      ami_type       = "AL2_x86_64"
+      ami_type       = "AL2023_x86_64"   # 🔥 REQUIRED for 1.33+
     }
   }
 }
